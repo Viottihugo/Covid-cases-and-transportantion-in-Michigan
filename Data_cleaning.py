08c5965e1b2819d0272183f074fc9fb55148e7ba
HViotti <viottihugo@gmail.com>
2020-11-08 14:06:19 -0300

    done with data cleaning of the trips

--- Data_cleaning.py
+++ Data_cleaning.py
@@ -24,9 +24,11 @@ print(covid_by_month)
 #trips info taken from https://data.bts.gov/Research-and-Statistics/Trips-by-Distance/w96p-f2qv
 trips_df=pd.read_csv('data\Trips_by_Distance.csv')
 
-trips_national=trips_df[trips_df['Level']=='National'].iloc[:,:-10]
+trips_MI=trips_df[trips_df['State Postal Code']=='MI'].iloc[:,:-10]
 
-trips_national=trips_national[pd.to_datetime(trips_national['Date'])>= '2020-03-01']
+trips_MI=trips_MI[pd.to_datetime(trips_MI['Date'])>= '2020-03-01']
 
+trips_MI.set_index('Date', inplace=True)
 
-	
+#saving the data to a csv file for later use
+(trips_MI.groupby(trips_MI.index).sum()).to_csv('data\Trips_MI.csv')
